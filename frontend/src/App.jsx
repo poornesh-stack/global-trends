e10312689737c4635b816256fb96bf87be1e1a62
intern1-lw <poorneshbs05@gmail.com>
2026-01-22 11:51:38 -0800

    Feature: Signup Page Design
    Designed the signup page for the app.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Dashboard from "./views/Dashboard";
 import PermanentDrawer from "./components/PermanentDrawer";
 import Crypto from "./views/Crypto";
@@ -7,12 +7,20 @@ import Currency from "./views/Currency";
 import Weather from "./views/Weather";
 import About from "./views/About";
 import Contact from "./views/Contact";
+import Login from "./views/Login";
+import Signup from "./views/Signup";
 
 export default function App() {
+  const location = useLocation();
+  const isAuthPage =
+    location.pathname === "/login" || location.pathname === "/signup";
+
   return (
     <>
-      <PermanentDrawer />
+      {!isAuthPage && <PermanentDrawer />}
       <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/" element={<Dashboard />} />
         <Route path="/crypto" element={<Crypto />} />
         <Route path="/currency" element={<Currency />} />
